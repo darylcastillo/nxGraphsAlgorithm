bfa6c0a0b0f0e1f466cafa994be0e442db034cc7
darylcastillo <49928141+darylcastillo@users.noreply.github.com>
2019-04-25 16:28:16 -0500

    Update bool_functions.py

--- nxGraph_Algorithm/Functions/bool_functions.py
+++ nxGraph_Algorithm/Functions/bool_functions.py
@@ -1,37 +1,33 @@
-import networkx as nx
-from Functions.local_properties import *
-from Functions.global_properties import *
+from global_properties import V
+from local_properties import neighbors
 
-
-def is_independent(G, S):
+def is_independent (G, S):
     for v in S:
         N = neighbors(G, v)
-        if list(set(N) & set(S)) != []:
+        if list(set(N) & set(S)) !=[]:
             return False
-    return True
-    
-
-def is_clique(G, s):
-    for i in range(len(s)):
-        N = neighbors(G, s[i])
-        for j in range(i + 1, len(s)):
-            if s[i + 1] not in N:
-                return False
     return True
 
 def is_dominating(G, S):
-    S_complement = list(set(V(G)) - set(S))
+    S_complement = list(set(V(G))-set(S))
     for v in S_complement:
         N = neighbors(G, v)
         if list(set(N) & set(S)) == []:
             return False
     return True
 
-def is_matching(G, M):
-    for edge1 in M:
-        v,w = edge1
-        for edge2 in M:
-            if edge2 != edge1:
-              if v in edge2 or w in edge2:
-                  return False
+def is_clique (G, S):
+    for v in S:
+        N = neighbors(G, v)
+        if list(set(N) & set(S)) !=[]:
+            return False
     return True
+
+def is_matching(G, m):
+    for edge1 in m:
+        v, w = edge1
+        for edge2 in m:
+            if edge2 != edge1:
+                if v in edge1 or w in edge1:
+                    return False
+    return True 
