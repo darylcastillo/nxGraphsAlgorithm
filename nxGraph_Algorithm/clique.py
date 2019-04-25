d588efb67491c374058fc164f3fe70f0298e2514
darylcastillo <49928141+darylcastillo@users.noreply.github.com>
2019-04-25 16:30:09 -0500

    Update clique.py

--- nxGraph_Algorithm/clique.py
+++ nxGraph_Algorithm/clique.py
@@ -1,13 +1,14 @@
-import networkx as nx
-from Functions.bool_functions import *
-from Functions.global_properties import *
+from functions.bool_functions import is_clique
+from functions.global_properties import n, V
 from itertools import combinations
 
+
 def maximum_clique(G):
-    for k in range(n(G), 1, -1):
+    for k in range (n(G), 1, -1):
         for S in combinations(V(G), k):
             if is_clique(G, list(S)) == True:
                 return list(S)
-            
+
+
 def clique_numbers(G):
     return len(maximum_clique(G))
